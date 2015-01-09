32bc11e95acffb0e4ab12277b6307f7cf3a3134f
Darren Shaw <shawdm@gmail.com>
2015-01-09 20:04:12 +0000

    upload directory writable

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -13,6 +13,8 @@ rm -rf /var/www/html
 ln -s /usr/share/wordpress /var/www/html
 cp /vagrant/bootstrap/wp-config-development.php /usr/share/wordpress/wp-config.php
 ln -s /vagrant/darrenshaw-org-wordpress-theme /usr/share/wordpress/wp-content/themes/darrenshaw-org-wordpress-theme
+mkdir /usr/share/wordpress/wp-content/uploads
+chmod 777 /usr/share/wordpress/wp-content/uploads
 apachectl restart
 
 # mysql create database
