37a5c0fbe2d042acc39d83458fa6949b6f16932a
Darren Shaw <shawdm@gmail.com>
2024-06-20 20:55:10 +0100

    Slideshow controls and contact details.

--- interactive/photo-slideshow/js/site-1.0.0.js
+++ interactive/photo-slideshow/js/site-1.0.0.js
@@ -1,6 +1,15 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+document.addEventListener("keydown", function onEvent(event) {
+  if (event.key === "ArrowLeft") {
+    plusSlides(-1);
+  }
+  else if (event.key === "ArrowRight") {
+    plusSlides(1);
+  }
+});
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -18,3 +27,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
