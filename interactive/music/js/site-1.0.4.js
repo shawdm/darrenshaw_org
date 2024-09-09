9a1901c023c19d224ed71cf5b08f96f76fba25c1
Darren Shaw <shawdm@gmail.com>
2024-09-09 19:38:43 +0100

    Added tabbed view of spotify music.

--- interactive/music/js/site-1.0.4.js
+++ interactive/music/js/site-1.0.4.js
@@ -1,8 +1,16 @@
-const POPULAR_URL = "data/music-library-api/popular/artist.json";
-const ALBUMS_URL = "data/music-library-api/album/current.json";
+const MIN_IMAGE_WIDTH = 200;
 
-loadPopular();
 loadAlbums();
+loadPlaylist();
+loadPopular();
+
+var HB_TEMPLATE_PLAYLIST = Handlebars.compile(
+    `
+    {{#each tracks}}
+        {{>track}}
+    {{/each}}
+   `
+);
 
 var HB_TEMPLATE_ARTISTS = Handlebars.compile(
     `
@@ -25,6 +33,18 @@ var HB_TEMPLATE_CHARACTERS = Handlebars.compile(
     {{/each}}
     `
 );
+
+Handlebars.registerPartial(
+    "track", 
+    `
+    <a href='{{url}}' title='#{{position}}: {{name}} - {{artist}}'>
+        <div class='image-frame artist'>
+            <img src='{{image}}'/>
+            <div class='description'>#{{position}} {{name}}</div>
+        </div>
+    </a>
+    `
+)
 
 Handlebars.registerPartial(
     "artist", 
@@ -66,6 +86,17 @@ Handlebars.registerPartial(
     `
 )
 
+function loadPlaylist(){
+    const options = {};
+    fetch(RECENT_PLAYLIST_URL, options)
+        .then(manageResponse)
+        .then(mapToPlaylistData)
+        .then(renderPlaylist)
+        .catch(error => {
+            console.log(`Error fetching url=${POPULAR_URL}`, error);
+        });
+}
+
 function loadPopular(){
     const options = {};
     fetch(POPULAR_URL, options)
@@ -99,13 +130,52 @@ function manageResponse(response){
     return response;    
 }
 
+async function mapToPlaylistData(response){
+    const json = await response.json();
+    const playlistTracks = json.playlistTracks
+        .map(track => {
+            return {
+                name: track.name,
+                artist: track.artists[0].name,
+                image: selectImageUrl(track.images),
+                url: `https://open.spotify.com/track/${track.id}`,
+                addedAt: Date.parse(track.addedAt)
+            };
+        })
+        .sort(playlistTrackComparator)
+        .map((track, index) => {
+            return {
+                name: track.name,
+                artist: track.artist,
+                image: track.image,
+                url: track.url,
+                addedAt: track.addedAt,
+                position: index+1
+            }
+        });
+    const updatedTimestamp = json.updatedTimestamp;
+
+    return {
+        tracks: playlistTracks,
+        updatedTimestamp: updatedTimestamp
+    };
+}
+
+function renderPlaylist(playlist){
+    const popularArtistsHtml = HB_TEMPLATE_PLAYLIST({tracks: playlist.tracks});
+    document.getElementById("music-list-playlist").innerHTML = popularArtistsHtml; 
+
+    const formattedDate = formatDate(playlist.updatedTimestamp);
+    const updatedDateHtml = HB_TEMPLATE_UPDATED_DATE({updatedDate: formattedDate});
+    document.getElementById("music-list-playlist-updated").innerHTML = updatedDateHtml; 
+}
+
 async function mapToArtistData(response){
     const json = await response.json();
-    
     const popularArtists = json.artists
         .map((spotifyArtist, index) => {return {
             name: spotifyArtist.name,
-            image: spotifyArtist.images[0].url,
+            image: selectImageUrl(spotifyArtist.images),
             url: `https://open.spotify.com/artist/${spotifyArtist.id}`,
             position: index+1
         }});
@@ -128,11 +198,8 @@ function renderPopularArtists(popularArtists){
 
 async function mapToAlbumData(response){
     const json = await response.json();
-    const updatedTimestamp = json.updatedTimestamp;
-
     const albumsGroupedAlphabetically = Object.groupBy(json.albums, (album) => album.artists[0].name.substring(0,1).toUpperCase());
     const characters = Object.keys(albumsGroupedAlphabetically).sort();
-
     const characterAlbums = characters
         .map(character => {return {
             character:character, 
@@ -140,12 +207,13 @@ async function mapToAlbumData(response){
                 .map(album => {return {
                     name: album.name,
                     artist: album.artists[0].name,
-                    image: album.images[0].url,
+                    image: selectImageUrl(album.images),
                     url: `https://open.spotify.com/album/${album.id}`,
                     year: album.releaseYear
                 }})
                 .sort(albumComparator)
         }});
+    const updatedTimestamp = json.updatedTimestamp;
 
     return {
         characters: characters,
@@ -172,6 +240,18 @@ function formatDate(dateString){
     return parsedDate.toLocaleDateString("en-GB", {dateStyle:"long"});
 }
 
+function selectImageUrl(images){
+    var selectedImage = images
+        .sort(imageSizeComparator)
+        .find(image => image.width >= MIN_IMAGE_WIDTH)
+    
+    if(selectedImage === undefined){
+        selectedImage = images[0];
+    }
+
+    return selectedImage.url
+}
+
 function albumComparator(a1, a2){
     const a1Artist = a1.artist.toLowerCase();
     const a2Artist = a2.artist.toLowerCase();
@@ -195,4 +275,48 @@ function albumComparator(a1, a2){
             return -1;
         }
     }
+}
+
+function playlistTrackComparator(t1, t2){
+  return t2.addedAt - t1.addedAt;
+}
+
+function imageSizeComparator(i1, i2){
+    return i1.width - i2.width;
+  }
+
+function showAlbums(){
+    document.getElementById("article-albums").classList.remove("hidden");
+    document.getElementById("article-playlist").classList.add("hidden");
+    document.getElementById("article-popular").classList.add("hidden");
+    
+    document.getElementById("music-nav-albums").classList.add("selected")
+    document.getElementById("music-nav-playlist").classList.remove("selected")
+    document.getElementById("music-nav-popular").classList.remove("selected")
+
+    return false
+}
+
+function showPlaylists(){
+    document.getElementById("article-albums").classList.add("hidden");
+    document.getElementById("article-playlist").classList.remove("hidden");
+    document.getElementById("article-popular").classList.add("hidden");
+    
+    document.getElementById("music-nav-albums").classList.remove("selected")
+    document.getElementById("music-nav-playlist").classList.add("selected")
+    document.getElementById("music-nav-popular").classList.remove("selected")
+
+    return false;
+}
+
+function showPopular(){
+    document.getElementById("article-albums").classList.add("hidden");
+    document.getElementById("article-playlist").classList.add("hidden");
+    document.getElementById("article-popular").classList.remove("hidden");
+    
+    document.getElementById("music-nav-albums").classList.remove("selected")
+    document.getElementById("music-nav-playlist").classList.remove("selected")
+    document.getElementById("music-nav-popular").classList.add("selected")
+
+    return false;
 }
